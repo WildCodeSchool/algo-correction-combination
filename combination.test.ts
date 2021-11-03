db8fbc4d242f6c2a41e2854c0d11d835ef936a79
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-11-03 15:25:11 +0100

    ready for lc

--- combination.test.ts
+++ combination.test.ts
@@ -1,16 +1,16 @@
-import combination, { combine } from "./combination"
+import combination from "./combination"
 
 describe('combine', () => {
-    it('combine one char with chars', () => {
-        expect(combine('a', ['a','b','c'], 3)).toMatchSnapshot()
-    })
+    // it('combine one char with chars', () => {
+    //     expect(combine('a', ['a','b','c'], 3)).toMatchSnapshot()
+    // })
 })
 
 describe('combination', () => {
     it('combinate all chars', () => {
         expect(combination(['a','b','c'], 2)).toEqual(['a','aa','ab','ac','b','ba','bb','bc','c','ca','cb','cc'])
     })
-    it('combinate all chars', () => {
-        expect(combination(['a','b','c'], 3)).toMatchSnapshot()
-    })
+    // it('combinate all chars', () => {
+    //     expect(combination(['a','b','c'], 3)).toMatchSnapshot()
+    // })
 })
