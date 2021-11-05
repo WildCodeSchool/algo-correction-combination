4de1f962ef19cac33045a4eeb38773b0ff852906
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-11-05 14:11:18 +0100

    reduce or for

--- combination.ts
+++ combination.ts
@@ -1,5 +1,22 @@
 export const combine = (char: string, chars: string[], length: number): string[] => {
 
+    // Solution using for to parse chars array
+    // let result = [char]
+
+    // for(let i = 0; i < chars.length; i++) {
+    //     let code = char + chars[i]
+
+    //     if(length > 2) {
+    //         result = [...result, ...combine(code, chars, length-1)]
+    //     } else {
+    //         result.push(code)
+    //     }
+    // }
+
+    // return result
+
+
+    // Solution using a reduce to parse chars array and accumulate a result
     const result2 = chars.reduce<string []>(
         (acc, value) => {
             const code = char + value
