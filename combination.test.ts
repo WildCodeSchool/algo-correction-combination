d7dc366c46addb97218e6e4799c3157394cefcde
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-11-05 13:28:48 +0100

    solution

--- combination.test.ts
+++ combination.test.ts
@@ -1,16 +1,56 @@
-import combination from "./combination"
+import combination, { combine } from "./combination"
 
 describe('combine', () => {
-    // it('combine one char with chars', () => {
-    //     expect(combine('a', ['a','b','c'], 3)).toMatchSnapshot()
-    // })
+    it('combine one char with chars', () => {
+        expect(combine('a', ['a','b','c'], 3)).toEqual(['a','aa','aaa', 'aab', 'aac','ab','aba','abb','abc','ac','aca','acb','acc'])
+    })
 })
 
 describe('combination', () => {
-    it('combinate all chars', () => {
+    it('combinate all chars with length 2', () => {
         expect(combination(['a','b','c'], 2)).toEqual(['a','aa','ab','ac','b','ba','bb','bc','c','ca','cb','cc'])
     })
-    // it('combinate all chars', () => {
-    //     expect(combination(['a','b','c'], 3)).toMatchSnapshot()
-    // })
+    it('combinate all chars  with length 3', () => {
+        expect(combination(['a','b','c'], 3)).toEqual([
+            "a",
+            "aa",
+            "aaa",
+            "aab",
+            "aac",
+            "ab",
+            "aba",
+            "abb",
+            "abc",
+            "ac",
+            "aca",
+            "acb",
+            "acc",
+            "b",
+            "ba",
+            "baa",
+            "bab",
+            "bac",
+            "bb",
+            "bba",
+            "bbb",
+            "bbc",
+            "bc",
+            "bca",
+            "bcb",
+            "bcc",
+            "c",
+            "ca",
+            "caa",
+            "cab",
+            "cac",
+            "cb",
+            "cba",
+            "cbb",
+            "cbc",
+            "cc",
+            "cca",
+            "ccb",
+            "ccc",
+          ])
+    })
 })
--- combination.ts
+++ combination.ts
@@ -1,17 +1,25 @@
 export const combine = (char: string, chars: string[], length: number): string[] => {
+
     let result = [char]
-    for (let i = 0; i < chars.length; i++) {
-        let code = char + chars[i]    
-        if (length > 2) {
-            result = result.concat(combine(code, chars, length-1))
+
+    for(let i = 0; i < chars.length; i++) {
+        let code = char + chars[i]
+
+        if(length > 2) {
+            result = [...result, ...combine(code, chars, length-1)]
         } else {
             result.push(code)
         }
     }
+
     return result
 }
+
+
 const combination = (chars: string[], length: number):string[] => {
-    return chars.flatMap(char => combine(char, chars, length)).sort()
+    return chars.map(
+        char => combine(char, chars, length)
+    ).flat(2).sort()
 }
 
 export default combination
